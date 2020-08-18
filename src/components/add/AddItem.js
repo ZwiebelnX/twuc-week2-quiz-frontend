38107ed05f4d3b9604e4371bd6a9ca12ea9eabf9
Chen Sicong <sicong.chen@163.com>
2020-08-18 23:30:34 +0800

    [Chen Sicong]refactor: item form and http request methods

--- src/components/add/AddItem.js
+++ src/components/add/AddItem.js
@@ -1,73 +1,73 @@
 import React from "react";
 import './AddItem.css'
-import {Button, Form, message} from "antd";
-import  axios from "axios";
+import {Button, Form, Input, message, Spin, InputNumber} from "antd";
+import 'antd/dist/antd.css'
 import {urls} from "../../utils/urls";
+import httpClient, {isSuccessRequest} from "../../utils/https";
 
 class AddItem extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            name: '',
-            price: '',
-            unit: '',
-            imageUrl: ''
-        };
-        this.submitInfo = async () => {
-            const data = {
-                name: this.state.name,
-                price: this.state.price,
-                unit: this.state.unit,
-                imageUrl: this.state.imageUrl
-            }
-            const response = await axios.post(urls.postItem(), data);
-            if (response.status === 200) {
-                message.success("添加商品成功");
-                this.setState({
-                    name: '',
-                    price: '',
-                    unit: '',
-                    imageUrl: ''
-                });
-            } else {
-                message.error("添加失败，请重试");
-            }
-        }
+        this.formRef = React.createRef()
 
-        this.handleNameChange = (event) => {this.setState({name: event.target.value})}
-        this.handlePriceChange = (event) => {this.setState({price: event.target.value})}
-        this.handleUnitChange = (event) => {this.setState({unit: event.target.value})}
-        this.handleImageUrlChange = (event) => {this.setState({imageUrl: event.target.value})}
+        this.state = {
+            isLoading: false,
+        };
+        this.submitInfo = async (value) => {
+            this.setState({isLoading: true})
+            try {
+                const response = await httpClient.post(urls.postItem(), value);
+                if (isSuccessRequest(response.status)) {
+                    message.success("添加商品成功");
+                    this.formRef.current.resetFields()
+                }
+            } catch (e) {
+                console.log(e)
+            }
+            this.setState({isLoading: false})
+        }
     }
 
     render() {
+        const formItemLayout = {labelCol: {span: 4}, wrapperCol: {span: 18}}
         return (
             <div className='add-item-container'>
-                <h1>添加商品</h1>
-                <Form onFinish={this.submitInfo}>
-                    <Form.Item label='商品名'
-                               name='name'
-                               rules={[{required: true, message: '请输入名称'}]}>
-                        <input value={this.state.name} onChange={this.handleNameChange} />
-                    </Form.Item>
-                    <Form.Item label='价格'
-                               name='price'
-                               rules={[{required: true, message: '价格'}]}>
-                        <input value={this.state.price} onChange={this.handlePriceChange} />
-                    </Form.Item>
-                    <Form.Item label='单位'
-                               name='unit'
-                               rules={[{required: true, message: '单位'}]}>
-                        <input  value={this.state.unit} onChange={this.handleUnitChange}/>
-                    </Form.Item>
-                    <Form.Item label='图片URL'
-                               name='imageUrl'
-                               rules={[{required: true, message: '请输入图片url'}]}>
-                        <input  value={this.state.imageUrl} onChange={this.handleImageUrlChange}/>
-                    </Form.Item>
-                    <Button type="primary" htmlType="submit">提交</Button>
-                </Form>
+                <Spin spinning={this.state.isLoading}>
+                    <h1>添加商品</h1>
+                    <Form {...formItemLayout}
+                          size='large'
+                          onFinish={this.submitInfo}
+                          ref={this.formRef}
+                          name='form'>
+                        <Form.Item label='商品名'
+                                   name='name'
+                                   rules={[{required: true, message: '请输入名称'}]}>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item label='价格'
+                                   name='price'
+                                   rules={[{required: true, message: '请输入价格'}
+                                   , {type: 'number', min: 1, message: '请输入大于0的数字'}]}>
+                            <InputNumber min='1' size='large'/>
+                        </Form.Item>
+                        <Form.Item label='单位'
+                                   name='unit'
+                                   rules={[{required: true, message: '请输入单位'}]}>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item label='图片URL'
+                                   name='imageUrl'
+                                   rules={[{validator: (_, value) =>
+                                           value && value.match(/^(https?:\/\/)/) ?
+                                               Promise.resolve() : Promise.reject('请输入正确的url')}]}>
+                            <Input/>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit">提交</Button>
+                        </Form.Item>
+                    </Form>
+                </Spin>
             </div>
         );
     }
--- src/utils/https.js
+++ src/utils/https.js
@@ -1,20 +1,27 @@
 import axios from "axios";
-import  { message} from "antd";
+import  {message} from "antd";
 
-const service = axios.create({
+const httpClient = axios.create({
     baseURL: "http://localhost:8080",
     timeout: 50000
 });
 
-service.interceptors.response.use(
+httpClient.interceptors.response.use(
     (response) => {
+        console.log(response)
         return response;
     },
     (error) => {
         console.error("response in error:" + error);
         let response = error.response
-        const errMessage = response ? `状态码：${response.status}；错误码：${response.data.code}；错误内容：${response.data.message}` : ''
+        const errMessage = response ? `状态码：${response.status}；错误码：${response.data.errorCode}；错误内容：${response.data.message}` : ''
         message.error(errMessage)
         return error.response;
     }
 );
+
+export const isSuccessRequest = (code) => {
+    return code >= 200 && code < 300;
+}
+
+export default httpClient;
